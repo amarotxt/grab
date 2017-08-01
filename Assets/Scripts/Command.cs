bfd6c347fdac84d71e93d988da4bb2a03c475ebc
Amaro <amaro.txt@gmail.com>
2017-08-01 18:04:58 -0300

    Script Player movimentão basica

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -3,8 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class Command {
+	protected float movimentSpeed = 5;
+	protected float forceJump = 10f;
+	protected float forceHighJump = 20f;
 
+
+	public abstract void Execute (GameObject player);
 	public virtual void Move (Transform player){ }
 	public virtual void Jump (Rigidbody player){ }
+
 //	public virtual void Hook(){ }
+}
+public class MoveLeft : Command{
+	public override void Execute(GameObject player){
+		Move (player.transform);
+	}
+	public override void Move (Transform player){
+		player.transform.Translate (new Vector3(-(Time.deltaTime*movimentSpeed), 0,0));
+	}
+}
+public class MoveRight : Command{
+	public override void Execute(GameObject player){
+		Move (player.transform);
+	}
+	public override void Move (Transform player){
+		player.transform.Translate (new Vector3(Time.deltaTime*movimentSpeed, 0,0));
+	}
+}
+public class MoveUp : Command{
+	public override void Execute(GameObject player){
+		Move (player.transform);
+	}
+	public override void Move (Transform player){
+		player.transform.Translate (new Vector3(0,(Time.deltaTime*movimentSpeed),0));
+	}
+}
+public class MoveDown : Command{
+	public override void Execute(GameObject player){
+		Move (player.transform);
+	}
+	public override void Move (Transform player){
+		player.transform.Translate (new Vector3(0,-(Time.deltaTime*movimentSpeed),0));
+	}
+}
+//public class JumpLeft : Command{
+//	public override void Execute(GameObject player){
+//		Jump (player.GetComponent<Rigidbody>());
+//	}
+//	public override void Jump (Rigidbody player){
+//		player.AddForce (new Vector3 (-(forceJump),forceJump, 0));
+//	}
+//}
+//public class JumpRight : Command{
+//	public override void Execute(GameObject player){
+//		Jump (player.GetComponent<Rigidbody>());
+//	}
+//	public override void Jump (Rigidbody player){
+//		player.AddForce (new Vector3 ( forceJump,forceJump, 0));
+//	}
+//}
+public class JumpUp : Command{
+	public override void Execute(GameObject player){
+		Jump (player.GetComponent<Rigidbody>());
+	}
+	public override void Jump (Rigidbody player){
+		player.AddForce ((Vector3.up*forceJump),ForceMode.Impulse);
+	}
+}
+
+public class JumpUpHigh : Command{
+	public override void Execute(GameObject player){
+		Jump (player.GetComponent<Rigidbody>());
+	}
+	public override void Jump (Rigidbody player){
+		player.AddForce ((Vector3.up*forceHighJump), ForceMode.Impulse);
+	}
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	public float speedZ;
+	public float aceleration;
+	GameObject player;
+	Command MoveRight, MoveLeft, MoveUp, MoveDown, JumpLeft, JumpRight, JumpUp, JumpUpHigh;
+	bool inGround;
+
+	void Start () {
+		speedZ = 0.1f;
+		aceleration = 1;
+		inGround = false;
+		player = GameObject.Find ("Player");
+		InvokeRepeating ("IncreaseSpeed",0.5f, 5.0f);
+		MoveRight = new MoveRight ();
+		MoveLeft = new MoveLeft ();
+		JumpUp = new JumpUp ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			MoveLeft.Execute (player);
+		}
+		if (Input.GetKey (KeyCode.RightArrow) ||Input.GetKey(KeyCode.D)) {
+			MoveRight.Execute (player);
+		}
+		if (Input.GetKeyDown(KeyCode.Space) && inGround){
+			JumpUp.Execute(player);
+		}
+
+		gameObject.transform.Translate (0,0,speedZ);
+		if (speedZ > 5) {
+			CancelInvoke();
+		}	
+	}
+
+	void IncreaseSpeed(){
+		speedZ += (Time.deltaTime * aceleration);
+	}
+
+
+	void OnCollisionEnter (Collision collider){
+		inGround = true;
+		if (collider.gameObject.CompareTag ("scaleCub")) {
+			deactvateGravity ();
+		}
+
+	}
+	void OnCollisionExit (Collision collider){
+		inGround = false;
+		if (collider.gameObject.CompareTag ("scaleCub")) {
+			deactvateGravity ();
+		}
+
+	}
+	void deactvateGravity(){
+		player.GetComponent<Rigidbody> ().useGravity = false; 
+	}
+	void actvateGravity(){
+		player.GetComponent<Rigidbody> ().useGravity = true; 
+	}
+
+}
