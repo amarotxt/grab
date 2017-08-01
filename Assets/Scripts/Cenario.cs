3b2ac278b64ca8e0edd73871de045e3fe430f372
Amaro <amaro.txt@gmail.com>
2017-08-01 01:52:19 -0300

    Iniciando Scripts

---
+++ Assets/Scripts/Cenario.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cenario : MonoBehaviour {
+
+
+	Transform player;
+	// Update is called once per frame
+	void Start (){
+		player = GameObject.Find("Player").GetComponent<Transform>();
+	}
+	void FixedUpdate () {
+		//Seguir jogador
+		gameObject.transform.position = new Vector3 (0,0,player.transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command {
+
+	public virtual void Move (Transform player){ }
+	public virtual void Jump (Rigidbody player){ }
+//	public virtual void Hook(){ }
+}
