a7551f4b99eec1fb988ec77abc9f87077f764b6b
sergiofffilho <sergiofffilho@gmail.com>
2017-08-02 17:09:48 -0300

    Tentativa de atirar gancho para frente

--- Assets/Scripts/Disparador.cs
+++ Assets/Scripts/Disparador.cs
@@ -27,13 +27,33 @@ public class Disparador : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		posMouse = Input.mousePosition;
-		posMouse.z = Vector3.Distance (m_camera.transform.position, transform.position);
-		posMouse = m_camera.ScreenToWorldPoint (posMouse);
+		Debug.Log (posMouse);
+
+
+
+//		posMouse.z = RaycastHit
+//		posMouse = m_camera.ScreenToWorldPoint (posMouse);
+
 
 		if (auxGancho == null) {
 			if (Input.GetMouseButtonDown (0)) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				if (Physics.Raycast (ray, out hit)) {
+					if (hit.collider != null) {
+						Debug.DrawRay ();
+						posMouse.z = hit.collider.gameObject.transform.position.z;
+						posMouse = m_camera.ScreenToWorldPoint (posMouse);
+						Debug.Log (posMouse);
+
+					}
+				}
+
 				auxDirDoClique = Instantiate (dirDoClique, posMouse, Quaternion.identity) as Transform;
+				Debug.Log (localDoClique);
+
 				localDoClique = (auxDirDoClique.transform.position - transform.position).normalized;
+				Debug.Log (localDoClique);
 				olharParaDir = Quaternion.LookRotation (localDoClique);
 
 				auxGancho = Instantiate (gancho, transform.position, olharParaDir) as GameObject;
