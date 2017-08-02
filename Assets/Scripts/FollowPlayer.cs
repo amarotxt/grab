2bfdc07278c01dddc71a713e87f20ac6610e2d45
Amaro <amaro.txt@gmail.com>
2017-08-02 14:40:59 -0300

    gancho, fllowplayer

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour{
+	Player player;
+	// Use this for initialization
+	void Start () {
+		player	= GameObject.Find ("Player").GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3(player.transform.position.x,player.transform.position.y,player.transform.position.z+player.speedZ);
+	}
+}
